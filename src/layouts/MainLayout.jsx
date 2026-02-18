69e532ac6e48b66556c29bc82ae5ee74e9c8e160
Rajat <rajat804@gmail.com>
2026-02-18 15:18:50 +0530

    main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,13 +5,23 @@ import Dashboard from "../pages/Dashboard";
 
 const MainLayout = () => {
   const [activeModule, setActiveModule] = useState("operations");
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div className="flex flex-col h-screen bg-gray-100">
-      <Topbar setActiveModule={setActiveModule} />
+      
+      <Topbar 
+        setActiveModule={setActiveModule} 
+        setSidebarOpen={setSidebarOpen}
+      />
 
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar activeModule={activeModule} />
+      <div className="flex flex-1 overflow-hidden relative">
+        
+        <Sidebar 
+          activeModule={activeModule}
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
 
         <div className="flex-1 p-6 overflow-auto">
           <Dashboard />
