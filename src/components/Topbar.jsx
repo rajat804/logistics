b1619946bb36d282c1ca074d2494391fb95b6a61
Rajat <rajat804@gmail.com>
2026-02-18 15:18:42 +0530

    top bar update

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,19 +1,25 @@
 import { Bell, Search, User, Menu } from "lucide-react";
 
-const Topbar = ({ setActiveModule }) => {
+const Topbar = ({ setActiveModule, setSidebarOpen }) => {
   return (
     <div className="bg-white shadow-md px-6 py-3 flex justify-between items-center">
       
       {/* Left Section */}
       <div className="flex items-center gap-4">
-        <Menu className="cursor-pointer text-slate-600" />
+        
+        {/* Mobile Toggle Button */}
+        <Menu
+          className="cursor-pointer text-slate-600 md:hidden"
+          onClick={() => setSidebarOpen(prev => !prev)}
+        />
+
         <h1 className="text-lg font-semibold text-slate-700">
           Golden Logistics ERP
         </h1>
 
         <select
           onChange={(e) => setActiveModule(e.target.value)}
-          className="ml-4 border px-3 py-1 rounded-md"
+          className="ml-4 border px-3 py-1 rounded-md hidden md:block"
         >
           <option value="operations">Operations</option>
           <option value="accounts">Accounts</option>
